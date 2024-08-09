8dbedf63b862d697ed88cf50c72bc258cb494b59
Haris Nadeem <nadeemharis810@gmail.com>
2024-08-09 10:48:36 +0500

    subtraction commit

--- addition.py
+++ addition.py
@@ -1,8 +1,8 @@
-def add_numbers(a, b):
-    return a + b
+def subtraction(a, b):
+    return a - b
 
 # Example usage
 num1 = 5
 num2 = 3
-result = add_numbers(num1, num2)
-print(f"The sum of {num1} and {num2} is {result}.")
+result = subtraction(num1, num2)
+print(f"The Diff of {num1} and {num2} is {result}.")
